679f1e25d9b5fd0bdf29f68084f46953e20ab8d0
Keren Duchan <keren.duchan@gmail.com>
2023-11-08 19:59:49 +0200

    refactor: use grid for graphic1

--- src/cmp/Graphic1.jsx
+++ src/cmp/Graphic1.jsx
@@ -3,11 +3,11 @@ import { Stripes } from './Stripes'
 export function Graphic1() {
     return (
         <div className="graphic1">
-            <div className="graphic1-item circle bg-secondary-2" />
-            <Stripes classes="graphic1-item" />
-            <div className="graphic1-item donut border-secondary-4" />
-            <div className="graphic1-item donut border-primary-2" />
-            <div className="graphic1-item bg-secondary-1" />
+            <div className="graphic1-full-circle circle bg-secondary-2" />
+            <Stripes />
+            <div className="graphic1-donut-back donut border-secondary-4" />
+            <div className="graphic1-donut-front donut border-primary-2" />
+            <div className="graphic1-square bg-secondary-1" />
         </div>
     )
 }
