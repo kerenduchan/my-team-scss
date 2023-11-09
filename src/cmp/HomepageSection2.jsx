af4f445643393d58399025d71ff5ee7421012ba9
Keren Duchan <keren.duchan@gmail.com>
2023-11-09 21:37:43 +0200

    wip homepage section 2

--- src/cmp/HomepageSection2.jsx
+++ src/cmp/HomepageSection2.jsx
@@ -7,7 +7,35 @@ export function HomepageSection2() {
                 </div>
                 <img className="stripe" src="images/stripe-primary-2.svg" />
                 <img className="pattern-3" src="images/bg-pattern-home-3.svg" />
+
+                <ul>
+                    <ListItem
+                        img="images/icon-person.svg"
+                        title="Experienced Individuals"
+                        text="Our network is made up of highly experienced professionals who are passionate about what they do."
+                    />
+                    <ListItem
+                        img="images/icon-cog.svg"
+                        title="Easy to Implement"
+                        text="Our processes have been refined over years of implementation meaning our teams always deliver."
+                    />
+                    <ListItem
+                        img="images/icon-chart.svg"
+                        title="Enhanced Productivity"
+                        text="Our customized platform with in-built analytics helps you manage your distributed teams."
+                    />
+                </ul>
             </div>
         </div>
+    )
+}
+
+function ListItem({ img, title, text }) {
+    return (
+        <li>
+            <img className="icon" src={img} />
+            <h3 className="title primary-2">{title}</h3>
+            <div className="text body-2">{text}</div>
+        </li>
     )
 }
