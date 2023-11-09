a3f49c6a99bb8a42d1f95bd016d80b8c265ab24e
Keren Duchan <keren.duchan@gmail.com>
2023-11-09 20:44:55 +0200

    pixel perfect header and first section

--- src/cmp/Header.jsx
+++ src/cmp/Header.jsx
@@ -1,12 +1,14 @@
 export function Header() {
     return (
-        <header>
-            <img className="logo" src="images/logo.svg" alt="myteam" />
-            <nav>
-                <a>home</a>
-                <a>about</a>
-            </nav>
-            <button className="contact-us">contact us</button>
+        <header className="bg-primary-3">
+            <div className="header-content">
+                <img className="logo" src="images/logo.svg" alt="myteam" />
+                <nav>
+                    <a>home</a>
+                    <a>about</a>
+                </nav>
+                <button className="contact-us">contact us</button>
+            </div>
         </header>
     )
 }
