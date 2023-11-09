5faa96ef092a57e6f5d0f63abe8abdd52474dd26
Keren Duchan <keren.duchan@gmail.com>
2023-11-09 21:13:39 +0200

    wip section 2

--- src/cmp/HomepageSection2.jsx
+++ src/cmp/HomepageSection2.jsx
@@ -1,11 +1,13 @@
 export function HomepageSection2() {
     return (
-        <div className="homepage-section-2">
-            <div className="title">
-                Build & manage distributed teams like no one else.
+        <div className="homepage-section-2 bg-secondary-4">
+            <div className="homepage-section-2-content">
+                <div className="title">
+                    Build & manage distributed teams like no one else.
+                </div>
+                <img className="stripe" src="images/stripe-primary-2.svg" />
+                <img className="pattern-3" src="images/bg-pattern-home-3.svg" />
             </div>
-            <img className="stripe" src="images/stripe-primary-2.svg" />
-            <img className="pattern-3" src="images/bg-pattern-home-3.svg" />
         </div>
     )
 }
