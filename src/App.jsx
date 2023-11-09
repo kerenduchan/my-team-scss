e682c4f2f13e0330c7197be8a78014545dc12bd3
Keren Duchan <keren.duchan@gmail.com>
2023-11-09 17:00:55 +0200

    wip homepage section 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from './cmp/Header'
 import { HomepageSection1 } from './cmp/HomepageSection1'
+import { HomepageSection2 } from './cmp/HomepageSection2'
 
 function App() {
     return (
         <>
             <Header />
             <HomepageSection1 />
+            <HomepageSection2 />
         </>
     )
 }
---
+++ src/cmp/HomepageSection2.jsx
@@ -0,0 +1,11 @@
+export function HomepageSection2() {
+    return (
+        <div className="homepage-section-2">
+            <div className="title">
+                Build & manage distributed teams like no one else.
+            </div>
+            <img className="stripe" src="stripe-primary-2.svg" />
+            <img className="graphic3" src="graphic3.svg" />
+        </div>
+    )
+}
