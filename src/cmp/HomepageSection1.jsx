d9c72544c361b9ed7c472460f28cf5f287ca6105
Keren Duchan <keren.duchan@gmail.com>
2023-11-10 13:55:11 +0200

    refactor homepage section 1 scss

--- src/cmp/HomepageSection1.jsx
+++ src/cmp/HomepageSection1.jsx
@@ -1,20 +1,18 @@
 export function HomepageSection1() {
     return (
         <div className="homepage-section-1 bg-primary-3">
-            <div className="homepage-section-1-content">
-                <div className="title">
-                    Find the
-                    <br /> best <mark>talent</mark>
-                </div>
-                <p>
-                    Finding the right people and building high performing teams
-                    can be hard. Most companies aren't tapping into the
-                    abundance of global talent. We're about to change that.
-                </p>
-                <img className="stripe" src="images/stripe-secondary-1.svg" />
-                <img className="pattern-2" src="images/bg-pattern-home-2.svg" />
-                <img className="pattern-1" src="images/bg-pattern-home-1.svg" />
+            <div className="title">
+                Find the
+                <br /> best <mark>talent</mark>
             </div>
+            <p>
+                Finding the right people and building high performing teams can
+                be hard. Most companies aren't tapping into the abundance of
+                global talent. We're about to change that.
+            </p>
+            <img className="stripe" src="images/stripe-secondary-1.svg" />
+            <img className="pattern-2" src="images/bg-pattern-home-2.svg" />
+            <img className="pattern-1" src="images/bg-pattern-home-1.svg" />
         </div>
     )
 }
