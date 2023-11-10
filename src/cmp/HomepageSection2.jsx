af8057fe12ba34c284d1055d1eb8c69dbab1d410
Keren Duchan <keren.duchan@gmail.com>
2023-11-10 16:05:23 +0200

    refactor section 2 to use data array

--- src/cmp/HomepageSection2.jsx
+++ src/cmp/HomepageSection2.jsx
@@ -1,4 +1,25 @@
 export function HomepageSection2() {
+    const items = [
+        {
+            id: 1,
+            img: 'images/icon-person.svg',
+            title: 'Experienced Individuals',
+            text: 'Our network is made up of highly experienced professionals who are passionate about what they do.',
+        },
+        {
+            id: 2,
+            img: 'images/icon-cog.svg',
+            title: 'Easy to Implement',
+            text: 'Our processes have been refined over years of implementation meaning our teams always deliver.',
+        },
+        {
+            id: 3,
+            img: 'images/icon-chart.svg',
+            title: 'Enhanced Productivity',
+            text: 'Our customized platform with in-built analytics helps you manage your distributed teams.',
+        },
+    ]
+
     return (
         <div className="homepage-section-2 bg-secondary-4">
             <img className="pattern-3" src="images/bg-pattern-home-3.svg" />
@@ -8,21 +29,15 @@ export function HomepageSection2() {
             </div>
 
             <ul>
-                <ListItem
-                    img="images/icon-person.svg"
-                    title="Experienced Individuals"
-                    text="Our network is made up of highly experienced professionals who are passionate about what they do."
-                />
-                <ListItem
-                    img="images/icon-cog.svg"
-                    title="Easy to Implement"
-                    text="Our processes have been refined over years of implementation meaning our teams always deliver."
-                />
-                <ListItem
-                    img="images/icon-chart.svg"
-                    title="Enhanced Productivity"
-                    text="Our customized platform with in-built analytics helps you manage your distributed teams."
-                />
+                {items.map((item) => (
+                    <li key={item.id}>
+                        <ListItem
+                            img={item.img}
+                            title={item.title}
+                            text={item.text}
+                        />
+                    </li>
+                ))}
             </ul>
         </div>
     )
