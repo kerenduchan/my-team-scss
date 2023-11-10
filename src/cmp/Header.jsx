fdd282d3708e47bf24a96da700f2a076ab3a85e0
Keren Duchan <keren.duchan@gmail.com>
2023-11-10 14:28:52 +0200

    wip header

--- src/cmp/Header.jsx
+++ src/cmp/Header.jsx
@@ -1,14 +1,13 @@
 export function Header() {
     return (
         <header className="bg-primary-3">
-            <div className="header-content">
-                <img className="logo" src="images/logo.svg" alt="myteam" />
-                <nav>
-                    <a>home</a>
-                    <a>about</a>
-                </nav>
-                <button className="contact-us">contact us</button>
-            </div>
+            <img className="logo" src="images/logo.svg" alt="myteam" />
+            <nav>
+                <a>home</a>
+                <a>about</a>
+            </nav>
+            <button className="contact-us">contact us</button>
+            <img src="images/icon-hamburger.svg" className="hamburger" />
         </header>
     )
 }
