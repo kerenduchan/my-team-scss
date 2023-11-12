bdeabea1bebaea82f20d07ff772c28991623171b
Keren Duchan <keren.duchan@gmail.com>
2023-11-12 12:12:44 +0200

    about section 2 mobile and tablet

--- src/cmp/DirectorCard.jsx
+++ src/cmp/DirectorCard.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 
 export function DirectorCard({ director }) {
-    const { name, avatar, quote } = director
+    const { name, avatar, quote, socials } = director
 
     const [isOpen, setIsOpen] = useState(false)
 
     return (
         <div className={`director ${isOpen ? 'open' : ''}`}>
             <h3>{name}</h3>
+
             <img className="avatar" src={avatar} />
+
             <blockquote>{quote}</blockquote>
+
+            <ul className="socials">
+                {socials.map((s) => (
+                    <li key={s.id}>
+                        <a key={s.id} className={s.id} href={s.link} />
+                    </li>
+                ))}
+            </ul>
 
             <button
                 className="circle-button"
