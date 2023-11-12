db82de2f410a9be16a727d991d8b67c93dd8b4cb
Keren Duchan <keren.duchan@gmail.com>
2023-11-12 07:37:11 +0200

    add key to list

--- src/util.jsx
+++ src/util.jsx
@@ -2,6 +2,6 @@ export { createDivs }
 
 function createDivs(count, prefix = 'bg') {
     return [...Array(count).keys()].map((n) => (
-        <div className={`${prefix}-${n + 1}`}></div>
+        <div key={n} className={`${prefix}-${n + 1}`}></div>
     ))
 }
